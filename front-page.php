6dc64ce1187e13c6381d2c7057153f5aa6e316b7
Lisa <lisaqdebona@gmail.com>
2020-07-21 23:07:25 -0600

    Move instagram feeds up

--- front-page.php
+++ front-page.php
@@ -26,8 +26,8 @@ get_header(); ?>
 		include( locate_template('template-parts/hero.php') ); 
 		include( locate_template('template-parts/sponsored-posts.php') );
 		include( locate_template('template-parts/subscribe-bar.php') ); 
-		include( locate_template('template-parts/non-sticky-news.php') );
 		include( locate_template('template-parts/instagram-feeds.php') );
+		include( locate_template('template-parts/non-sticky-news.php') );
 		include( locate_template('template-parts/home-bottom.php') );
 		?>
 			
